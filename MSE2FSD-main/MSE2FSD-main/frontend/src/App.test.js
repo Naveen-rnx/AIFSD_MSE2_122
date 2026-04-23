a8abcaba2cb5988df1361a01f49004229bcd266d
Naveen-rnx <naveen852963@gmail.com>
2026-04-23 16:04:52 +0530

    Delete MSE2FSD-main/MSE2FSD-main directory

--- MSE2FSD-main/MSE2FSD-main/frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders login heading", () => {
-  localStorage.clear();
-  render(<App />);
-  expect(screen.getByText(/^login$/i)).toBeInTheDocument();
-});
